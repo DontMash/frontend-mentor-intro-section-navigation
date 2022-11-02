35f3cd99a7e107cc4fc4b24e661a40a4a6f6000c
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-02 11:56:31 +0100

    Initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      'almost-white': '#fafafa',
+      'medium-gray': '#696969',
+      'almost-black': '#141414',
+    },
+    fontFamily: {
+      'epilogue': ['Epilogue', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
